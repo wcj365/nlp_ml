9acbcb3ef7168a3cdbda88880d30b52be1ef1786
Dr. Chaojie Wang <wcj365@gmail.com>
2022-11-03 16:24:16 -0400

    minor improvements.

--- src/collect_data.py
+++ src/collect_data.py
@@ -7,7 +7,7 @@ import pandas as pd
 BATCH_SIZE = 1000    # Maximum allowed by OpenFDA is 1000
 FETCH_SIZE = 100000  # Enforcement report has around 29k records
 DATE_RANGE = "[20200101+TO+20221231]"   # This is the accepted format to specify report date range
-API_URL = f"https://api.fda.gov/device/enforcement.json?search=report_date:{DATE_RANGE}&limit={BATCH_SIZE}&skip="
+API_URL = "https://api.fda.gov/device/enforcement.json"
 
 def get_enforcement_data():
 
@@ -16,7 +16,7 @@ def get_enforcement_data():
     while True:
         print(f"Retriving batch #{i + 1}.", end=" ")
         skip = i * BATCH_SIZE
-        resp = requests.get(API_URL + str(skip))
+        resp = requests.get(f"{API_URL}?search=report_date:{DATE_RANGE}&limit={BATCH_SIZE}&skip={skip}")
         json_resp = json.loads(resp.text) 
 
         # OpenFDA imposes a limit on skip at 25000 meaning the maximum # of records retrived is 26000
@@ -48,8 +48,8 @@ if __name__ == "__main__":
 
     df = get_enforcement_data()
 
-    df.to_csv("./data/enforcement_reports.csv", index=False)
-
     print(df.shape)  
     print(df.info()) 
     print(df.head())
+
+    df.to_csv("../data/enforcement_reports.csv", index=False)
